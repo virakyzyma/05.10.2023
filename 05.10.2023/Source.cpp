d8a7e565249e304a280dd9f4aa00f903309d99c2
Vira Kyzyma <vera.kyzyma8@gmail.com>
2023-10-05 19:29:56 +0300

    new project

--- 05.10.2023/Source.cpp
+++ 05.10.2023/Source.cpp
@@ -1,19 +1,79 @@
 #include <iostream>
 using namespace std;
 
+class Passport {
 
-class Point
-{
-	int x;
-	int y;
-public: 
-	Point()
+protected:
+
+	string name;
+	string nationality;
+	int authority;
+	int age;
+public:
+	Passport() = default;
+	string& GetName() {
+		return name;
+	}
+	string& GetNationality() {
+
+		return nationality;
+
+	}
+	int& GetAuthority()
 	{
+		return authority;
+	}
+	int& GetAge() 
+	{
+		return age;
+	}
 
+};
+
+
+class ForeignPassport : public Passport {
+	int passportNumber;
+	string visa;
+public:
+	ForeignPassport(int W) {
+		passportNumber = W;
+	}
+	ForeignPassport(string W) {
+		visa = W;
+	}
+	int& GetPassportNumber() {
+		return passportNumber;
+	}
+	string& GetVisa() {
+		return visa;
+	}
+	void Show()
+	{
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Nationality: " << nationality << endl;
+		cout << "Passport number: " << passportNumber << endl;
+		cout << "Visa: " << visa << endl;
 	}
 };
 
+
+
 int main()
 {
-	cout << "hello";
+	//Создайте класс Passport (паспорт), который будет содержать паспортную информацию о гражданине Украины.
+	//С помощью механизма наследования реализуйте класс ForeignPassport(загранпаспорт), производный от Passport.
+	//Напомним, что загранпаспорт содержит помимо паспортных данных также данные о визах, номер загранпаспорта.
+
+	ForeignPassport A(1);
+
+	A.GetName() = "Anna";
+	A.GetAge() = 20;
+	A.GetNationality() = "Ukrain";
+	A.GetAuthority() = 1234;
+	A.GetPassportNumber() = 123456;
+	A.GetVisa() = "Your visa";
+	A.Show();
+
+
 }
